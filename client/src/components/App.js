73e8c3c85eadc7c6271bff3f75196efafcc8dbde
Pichai Tangtrongsakundee <Pichai.t@gmail.com>
2023-12-27 03:58:06 +1100

    Sixth commit

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,18 +3,18 @@ import React, {useState, useEffect} from 'react'
 function App() {
 
   const [backendData, setBackendData] = useState([{}]);
-  useEffect( () => {
-                    setBackendData({"users": ["Pichai1", "Pichai2", "Pichai3", "Pichai5  "]})           
-                   }, []);
-
   // useEffect( () => {
-  //   fetch("/api").then( 
-  //       response => response.json()).then(
-  //         data => {
-  //           setBackendData(data)
-  //         }
-  //       )
-  // }, []);
+  //                   setBackendData({"users": ["Pichai1", "Pichai2", "Pichai3", "Pichai5  "]})           
+  //                  }, []);
+
+  useEffect( () => {
+    fetch("/api").then( 
+        response => response.json()).then(
+          data => {
+            setBackendData(data)
+          }
+        )
+  }, []);
 
   return (
     <div>
