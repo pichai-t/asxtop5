24f6eab82dfd2e64006ba51911882f1e0fcc1fd3
Pichai Tangtrongsakundee <Pichai.t@gmail.com>
2023-12-27 02:43:53 +1100

    Second commit

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+import exp from "constants";
 import express from "express";
+import path from "path";
 
 const app = express();
 const SERVER_PORT = 5000;
@@ -6,6 +8,12 @@ const SERVER_PORT = 5000;
 app.get('/api', (req, res) => {
     res.json( {"users": ["Pichai1", "Pichai2", "Pichai3", "Pichai5  "]} ); 
 });
+
+// Using Static
+app.use(express.static("./client/build")); 
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client","build", "index.html"));
+})
 
 app.listen( SERVER_PORT, () => {
     console.log("Server has started on " + SERVER_PORT);
